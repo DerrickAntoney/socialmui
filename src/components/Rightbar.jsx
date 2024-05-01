19276e76553e5006355a22496ed7c3c0f49b51e7
Derrick Antoney <117768226+DerrickAntoney@users.noreply.github.com>
2024-05-01 03:18:36 +0300

    Corrects bug

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from '@mui/material'
+import { Avatar, AvatarGroup, Box, Typography } from '@mui/material'
 import React from 'react'
 
 const Rightbar = () => {
@@ -7,12 +7,12 @@ const Rightbar = () => {
       <Box position='fixed'>
         <Typography variant='h6' fontWeight={100}>Online Friends</Typography>
         <AvatarGroup max={4}>
-  <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-  <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-  <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-  <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
-  <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
-</AvatarGroup>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+          <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
+          <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+        </AvatarGroup>
       </Box>
     </Box>
   )
