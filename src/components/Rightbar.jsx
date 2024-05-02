455c8ab3efd8abd834254a8cd8ded0bef5d3b620
Derrick Antoney <117768226+DerrickAntoney@users.noreply.github.com>
2024-05-02 15:03:14 +0300

    Creates and styles latest conversation, friends and photos

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,18 +1,109 @@
-import { Avatar, AvatarGroup, Box, Typography } from '@mui/material'
+import { Avatar, AvatarGroup, Box, Divider, ImageList, ImageListItem, List, ListItem, ListItemAvatar, ListItemText, Typography } from '@mui/material'
 import React from 'react'
 
 const Rightbar = () => {
   return (
     <Box flex={2} p={2} sx={{display: {xs: 'none', sm: 'block'}}}>
-      <Box position='fixed'>
+      <Box position='fixed' width={300}>
         <Typography variant='h6' fontWeight={100}>Online Friends</Typography>
-        <AvatarGroup max={4}>
-          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-          <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
-          <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+        <AvatarGroup max={6}>
+          <Avatar alt="Remy Sharp" src="/static/avatar4.jpg" />
+          <Avatar alt="Travis Howard" src="/static/avatar5.jpg" />
+          <Avatar alt="Cindy Baker" src="/static/avatar3.jpg" />
+          <Avatar alt="Agnes Walker" src="/static/avatar1.jpg" />
+          <Avatar alt="Trevor Henderson" src="/static/avatar2.jpg" />
+          <Avatar alt="Agnes Walker" src="/static/avatar1.jpg" />
+          <Avatar alt="Trevor Henderson" src="/static/avatar2.jpg" />
         </AvatarGroup>
+        <Typography variant='h6' fontWeight={100} mt={2} mb={2}>Latest Photos</Typography>
+        <ImageList cols={3} rowHeight={100}>
+          <ImageListItem>
+            <img
+            src="/static/post1.jpg"
+            alt="post"
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+            src="/static/post2.jpg"
+            alt="post"
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+            src="/static/post3.jpg"
+            alt="post"
+            />
+          </ImageListItem>
+        </ImageList>
+        <Typography variant='h6' fontWeight={100} mt={2}>Latest Conversations</Typography>
+        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Remy Sharp" src="/static/avatar1.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Brunch this weekend?"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Ali Connors
+                  </Typography>
+                  {" — I'll be in your neighborhood doing errands this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Travis Howard" src="/static/avatar2.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Summer BBQ"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    to Scott, Alex, Jennifer
+                  </Typography>
+                  {" — Wish I could come, but I'm out of town this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Cindy Baker" src="/static/avatar3.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Oui Oui"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: 'inline' }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Sandra Adams
+                  </Typography>
+                  {' — Do you have Paris recommendations? Have you ever…'}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+        </List>
       </Box>
     </Box>
   )
